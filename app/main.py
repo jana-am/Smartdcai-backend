cb45aaa8069b1896b98368a6311a5c273f6837e5
jana-am <janaamairi@gmail.com>
2026-03-03 23:53:24 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,11 +32,11 @@ def predict(city_name: str):
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
-from fastapi import UploadFile, File
 from typing import List
+from fastapi import UploadFile, File
 
 @app.post("/predict/upload")
 async def predict_upload(
-    files: List[UploadFile] = File(...)
+    files: List[UploadFile] = File(..., description="Upload multiple CSV files")
 ):
     return await predict_from_uploaded_csvs(files)
