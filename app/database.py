e44e9fa4542e386d66794d467f395643cca2da01
jana-am <janaamairi@gmail.com>
2026-03-27 21:35:34 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -12,7 +12,6 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
 
