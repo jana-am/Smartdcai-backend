2e20ae61986faaf57b7447656e0642f6d08ac588
tayma21 <s0000003283@ud.ac.ae>
2026-05-09 15:05:58 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://smartdcai.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
